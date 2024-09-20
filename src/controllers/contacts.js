420cb53702d2650c456ce78da8b943ac92494716
BogushF <boga1602@gmail.com>
2024-09-20 09:54:54 +0200

    patch

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,6 +2,7 @@ import {
   createContact,
   getAllContacts,
   getContactById,
+  updateContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
@@ -34,6 +35,21 @@ export const createContactController = async (req, res) => {
   res.status(201).json({
     status: 201,
     message: `Successfully created a contact!`,
+    data: contact,
+  });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await updateContact(contactId, req.body);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.json({
+    status: 200,
+    message: `Contact with ${contactId} successfully updated`,
     data: contact,
   });
 };
