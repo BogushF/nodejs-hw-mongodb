9a37e85a667771ce475b6577a53912b052b77996
BogushF <boga1602@gmail.com>
2024-09-26 13:42:24 +0200

    parse

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,8 +2,8 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number().integer().min(6).max(12).required(),
-  email: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(6).max(16).required(),
+  email: Joi.string().min(3).max(20),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
     .default('personal')
@@ -13,7 +13,7 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.number().integer().min(6).max(12),
+  phoneNumber: Joi.number().integer().min(6).max(16),
   email: Joi.string().min(3).max(20),
   contactType: Joi.string().valid('work', 'home', 'personal'),
   isFavourite: Joi.boolean(),
